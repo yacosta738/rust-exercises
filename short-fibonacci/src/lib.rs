7045344c1fdf5128fedb96fa7b4dea9c2f4a0992
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2021-11-12 00:01:49 -0300

    shirt fibonacci

---
+++ short-fibonacci/src/lib.rs
@@ -0,0 +1,26 @@
+/// Create an empty vector
+pub fn create_empty() -> Vec<u8> {
+    Vec::new()
+}
+
+/// Create a buffer of `count` zeroes.
+///
+/// Applications often use buffers when serializing data to send over the network.
+pub fn create_buffer(count: usize) -> Vec<u8> {
+    vec![0; count]
+}
+
+/// Create a vector containing the first five elements of the Fibonacci sequence.
+///
+/// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
+/// Its first five elements are `1, 1, 2, 3, 5`.
+pub fn fibonacci() -> Vec<u8> {
+    //calculate the fibonacci sequence
+    let mut fibonacci_sequence = create_empty();
+    fibonacci_sequence.push(1);
+    fibonacci_sequence.push(1);
+    for i in 2..5 {
+        fibonacci_sequence.push(fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]);
+    }
+    fibonacci_sequence
+}
